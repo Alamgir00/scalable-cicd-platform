55c069a8220f59818770fff74c54d1ceacfcb5b4
Alamgir00 <46954227+Alamgir00@users.noreply.github.com>
2025-10-27 23:39:18 +0530

    Add AWS region and project variables to Terraform

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,1 +1,13 @@
-
+# infrastructure/variables.tf
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "project" {
+  type    = string
+  default = "demo-cicd"
+}
+variable "desired_count" {
+  type    = number
+  default = 1
+}
